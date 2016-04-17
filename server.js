ba807ba29c1680ce020dc4e864264dc49cab98f3
anand <imdking002@gmail.com>
2016-04-17 12:42:34 +0530

    Add socket on client and server

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express=require('express');
 var PORT=process.env.PORT||3000;
 var app=express();
 var http=require('http').Server(app);
+var io=require('socket.io')(http);
+
 
 app.use(express.static(__dirname+'/public'));
+io.on('connection',function(){
+	console.log('User connected via socket.io!!')
+});
+
+
+
 http.listen(PORT,function(){
 	console.log('Server started!');
 });
-
+//heroku login
+//heroku create  
+//git push heroku
+//heroku open
