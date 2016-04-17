03389c39fb90661bb6ec9d0da6e570d7c86662cb
anand <imdking002@gmail.com>
2016-04-17 15:46:45 +0530

    added ui changes

---
+++ public/js/app.js
@@ -0,0 +1,59 @@
+/*var remoteMessage;
+var socket=io();
+socket.on('connect',function(){
+	console.log('Connected')
+});
+socket.on('message',function(message){
+	console.log(message.text);
+	remoteMessage=message.text;
+});	*/
+//handles submmitting the new messages
+var app=angular.module('myApp',[]);
+
+app.controller('chatCtrl',function($scope,socket){
+	
+
+   $scope.messageFreind=[
+      'hii'
+                        ];
+socket.on('connect',function(){
+	console.log('Connected')
+});
+socket.on('message',function(message){
+	console.log(message.text);
+	$scope.messageFreind.push(message.text);
+});	
+
+$scope.addThis=function(){
+	socket.emit('message',{
+		text:$scope.text,
+
+		
+	});
+	$scope.myMessage=$scope.text
+}
+
+});
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
